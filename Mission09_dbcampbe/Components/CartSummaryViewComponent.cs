5c2d89a4b84ed25d047d99dc4968a2235e39dd29
Dallascampbell <dallasbcampbell@gmail.com>
2023-03-16 10:56:37 -0600

    Shopping cart icon

---
+++ Mission09_dbcampbe/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission09_dbcampbe.Models;
+
+namespace Mission09_dbcampbe.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+
+    }
+}
